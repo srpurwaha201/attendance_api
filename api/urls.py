a545cec49f8a204bf4ae4493cd1581a8dad696b4
Shivi R Purwaha <srpurwaha201@gmail.com>
2019-09-28 22:51:12 +0530

    created basic attendance overview api

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import AttendanceView
+from .views import AttendanceView, StudentView
 
 
 app_name = "attendances"
@@ -8,4 +8,5 @@ app_name = "attendances"
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
     path('attendances/', AttendanceView.as_view()),
+    path('student/', StudentView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .models import Attendance, Timetable, Student
-from .serializers import AttendanceSerializer
+from .serializers import AttendanceSerializer, SectionSerializer, StudentSerializer
 
 class AttendanceView(APIView):
     def get(self, request):
@@ -39,3 +39,14 @@ class AttendanceView(APIView):
         # return Response({"success": "Attendance for '{}' created successfully".format(attendance_saved.date)})
 
         return Response({"success":""})
+
+class StudentView(APIView):
+    def get(self, request):
+        print("roll no is -------------> ",request.data.get('rollno'))
+        rollno = request.data.get('rollno')
+        student = Student.objects.get(rollno = rollno)
+        # print("studet is ------------------------>",student.name)
+        sections = student.section_set.all()
+        studentserializer = StudentSerializer(student)
+        sectionserializer = SectionSerializer(sections, many=True)
+        return Response({"Student":studentserializer.data, "Sections":sectionserializer.data})
