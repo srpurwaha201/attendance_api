b334a4fc594517fe6f1ea274458c2632a2193a74
Shivi R Purwaha <srpurwaha201@gmail.com>
2019-09-28 19:54:28 +0530

    created models

---
+++ api/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+
+class Student(models.Model):
+    rollno = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50, null=False)
+
+
+class Teacher(models.Model):
+    name = models.CharField(max_length=50, null=False)
+
+class Subject(models.Model):
+    code = models.CharField(max_length=50, primary_key=True)
+    name = models.CharField(max_length=50, null=False)
+
+
+class Section(models.Model):
+    #will include slot like CO402-P
+    slot = models.CharField(primary_key=True, max_length=10)
+    subject = models.ForeignKey(Subject, null=False,on_delete=models.CASCADE)
+    students = models.ManyToManyField(Student,blank=False)
+    teacher = models.ForeignKey(Teacher,on_delete=models.CASCADE)
+
+
+class Timetable(models.Model):
+    section = models.ForeignKey(Section, null=False,on_delete=models.CASCADE)
+    day = models.CharField(max_length=10, null=False)
+    startTime = models.TimeField()
+    endTime = models.TimeField()
+
+class Attendance(models.Model):
+    timetable = models.ForeignKey(Timetable,on_delete=models.CASCADE)
+    date = models.DateField()
+    #students contain only present students. Total no. of class will be calculated using current date and timetable.
+    #total % will be calculated accordingly
+    students = models.ManyToManyField(Student)
