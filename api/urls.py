4af35f80cd76e1a5f4ec0b8db0872d9e65a3b87f
Shivi R Purwaha <srpurwaha201@gmail.com>
2019-09-28 20:58:02 +0530

    added rest_framework to installed_apps

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import AttendanceView
 
 
-app_name = "attendance"
+app_name = "attendances"
 
 # app_name will help us do a reverse look-up latter.
 urlpatterns = [
